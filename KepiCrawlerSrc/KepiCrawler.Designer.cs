6ec85edc192ae04d3766522173d01322ada1f3b1
AndreasDerFuchs <ask-giub14@schlack.ws>
2016-02-06 07:48:38 +0100

    Initial Version

---
+++ KepiCrawlerSrc/KepiCrawler.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace MyKepiCrawler
+{
+    partial class KepiCrawler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private bool m_DoExit = false;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KepiCrawler));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer_5min = new System.Windows.Forms.Timer(this.components);
+            this.label_last_update = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox_eMail = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.NotifyIconMenu = new System.Windows.Forms.ContextMenu();
+            this.timer_CntDwn = new System.Windows.Forms.Timer(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "RegEx:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "default string: see settings";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(936, 102);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(268, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancel / Reset / Delete Temp File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer_5min
+            // 
+            this.timer_5min.Enabled = true;
+            this.timer_5min.Interval = 300000;
+            this.timer_5min.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label_last_update
+            // 
+            this.label_last_update.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_last_update.AutoSize = true;
+            this.label_last_update.Location = new System.Drawing.Point(804, 18);
+            this.label_last_update.Name = "label_last_update";
+            this.label_last_update.Size = new System.Drawing.Size(62, 13);
+            this.label_last_update.TabIndex = 7;
+            this.label_last_update.Text = "last_update";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(936, 326);
+            this.webBrowser1.TabIndex = 5;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 42);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(936, 432);
+            this.splitContainer1.SplitterDistance = 102;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // textBox_eMail
+            // 
+            this.textBox_eMail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_eMail.Location = new System.Drawing.Point(452, 15);
+            this.textBox_eMail.Name = "textBox_eMail";
+            this.textBox_eMail.Size = new System.Drawing.Size(346, 20);
+            this.textBox_eMail.TabIndex = 9;
+            this.textBox_eMail.Text = "default email addresses: see settings";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Kepi Crawler läuft noch ...";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Kepi Crawler";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            this.notifyIcon1.ContextMenu = this.NotifyIconMenu;
+            // 
+            // NotifyIconMenu
+            // 
+            this.NotifyIconMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1});
+            // 
+            // timer_CntDwn
+            // 
+            this.timer_CntDwn.Interval = 1000;
+            this.timer_CntDwn.Tick += new System.EventHandler(this.timer_CntDwn_Tick);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.Text = "Exit";
+            this.menuItem1.Click += menuItem1_Click;
+            // 
+            // KepiCrawler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 486);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.textBox_eMail);
+            this.Controls.Add(this.label_last_update);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "KepiCrawler";
+            this.Text = "Kepi Crawler";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        void menuItem1_Click(object sender, System.EventArgs e)
+        {
+            m_DoExit = true;
+            System.Windows.Forms.Application.Exit();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer_5min;
+        private System.Windows.Forms.Label label_last_update;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox_eMail;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer_CntDwn;
+        private System.Windows.Forms.ContextMenu NotifyIconMenu;
+        private System.Windows.Forms.MenuItem menuItem1;
+    }
+}
+
---
+++ KepiCrawlerSrc/KepiCrawler.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MyKepiCrawler
+{
+
+    public partial class KepiCrawler : Form
+    {
+        private string tmpFile = "C:\\Temp\\ask_kepi_plan_auszug.txt";
+        private string subContent = "";
+        private int cntDown = 0;
+
+        public Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        public string RemoveBrackets(string s)
+        {
+            int i, j;
+            for (; ; )
+            {
+                i = s.IndexOf('<');
+                j = s.IndexOf('>');
+
+                if ((i < 0) || (j <= i))
+                    break;
+                s = s.Remove(i) + "\t" + s.Remove(0, j + 1);
+            }
+            return s;
+        }
+
+        public KepiCrawler()
+        {
+            string path_dir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "KepiCrawler");
+            string path_full = System.IO.Path.Combine(path_dir, Path.GetFileName(tmpFile));
+            if (!Directory.Exists(path_dir))
+            {
+                Directory.CreateDirectory(path_dir); //Needs to be created
+            }
+            if (Directory.Exists(Path.GetDirectoryName(tmpFile)))
+                File.WriteAllText(tmpFile, "see file: " + path_full);
+            tmpFile = path_full;
+
+            InitializeComponent();
+            FormClosing += KepiCrawler_FormClosing;
+            this.textBox2.Text = Properties.Settings.Default.MySearchString;
+            this.textBox_eMail.Text = Properties.Settings.Default.MySendEmail;
+            this.Resize += frmMain_Resize;
+            button1_Click(this, null);
+        }
+
+        private static int WM_QUERYENDSESSION = 0x11;
+        private static bool systemShutdown = false;
+        protected override void WndProc(ref System.Windows.Forms.Message m)
+        {
+            if (m.Msg == WM_QUERYENDSESSION)
+            {
+                // MessageBox.Show("KepiCrawler: this is a logoff, shutdown, or reboot");
+                systemShutdown = true;
+                Application.Exit();
+            }
+
+            // If this is WM_QUERYENDSESSION, the closing event should be
+            // raised in the base WndProc.
+            base.WndProc(ref m);
+
+        } //WndProc 
+
+
+        private void KepiCrawler_FormClosing(object sender, FormClosingEventArgs e)
+        {
+#if FALSE
+            var window = MessageBox.Show("Willst Du den Kepi Crawler wirklich beenden?\nBei 'Ja' läuft nichts mehr,\nbei 'Nein' wird er nur minimiert.", "Kepi Crawler beenden?",
+                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button2);
+            if (window == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+            else if (window == DialogResult.No)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+            }
+            else
+                e.Cancel = false;
+#else
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+            }
+            if (m_DoExit || systemShutdown)
+                e.Cancel = false;
+#endif
+        }
+        
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                this.notifyIcon1.Visible = true;
+                this.notifyIcon1.ShowBalloonTip(500);
+                this.Hide();
+            }
+#if FALSE
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                this.notifyIcon1.Visible = false;
+            }
+#endif
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "http://212.71.198.9/vp_home/VP_Web.php";
+            string postData = "user=kepler&passwort=1571-1630&submit=Anmelden";
+            string pageContent = "";
+
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+
+            myHttpWebRequest.Method = "POST";
+
+            byte[] data = Encoding.ASCII.GetBytes(postData);
+
+            myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            myHttpWebRequest.ContentLength = data.Length;
+
+            try
+            {
+                using (Stream requestStream = myHttpWebRequest.GetRequestStream())
+                {
+                    requestStream.Write(data, 0, data.Length);
+                    requestStream.Close();
+                }
+            }
+            catch (System.Net.WebException ex)
+            {
+                this.richTextBox1.Text = "GetRequestStream failed: " + ex.Message;
+                Thread.Sleep(1000);
+                return;
+            }
+
+            try
+            {
+                using (HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse())
+                using (Stream responseStream = myHttpWebResponse.GetResponseStream())
+                using (StreamReader myStreamReader = new StreamReader(responseStream, Encoding.Default))
+                {
+                    pageContent = myStreamReader.ReadToEnd();
+                    string searchString = this.textBox2.Text;
+
+                    subContent = "";
+                    using (Stream s = GenerateStreamFromString(pageContent))
+                    using (StreamReader myStreamReader2 = new StreamReader(s, Encoding.Default))
+                    {
+                        string derTag = "";
+
+                        // Define a case-insensitive regular expression for repeated words.
+                        // for usage see http://msdn.microsoft.com/en-us/library/az24scfc.aspx
+                        Regex rxInsensitive = new Regex(searchString, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+                        while (!myStreamReader2.EndOfStream)
+                        {
+                            string ln = myStreamReader2.ReadLine();
+                            if (ln.Contains("mon_title"))
+                                derTag = RemoveBrackets(ln) + '\n';
+                            // Find matches.
+                            MatchCollection matches = rxInsensitive.Matches(ln);
+                            if (matches.Count > 0)
+                            {
+                                ln = ln.Replace("&nbsp;", " "); // non-breaking space
+                                subContent += derTag + RemoveBrackets(ln) + '\n';
+                                derTag = "";
+                            }
+                        }
+                        myStreamReader2.Close();
+                    }
+
+                    myStreamReader.Close();
+                    responseStream.Close();
+                    myHttpWebResponse.Close();
+                }
+            }
+            catch (System.Net.ProtocolViolationException ex)
+            {
+                this.richTextBox1.Text = "GetResponse failed: " + ex.Message;
+                Thread.Sleep(1000);
+                return;
+            }
+
+            this.webBrowser1.DocumentText = pageContent;
+            this.richTextBox1.Text = subContent;
+
+            string oldSubContent = "";
+            try
+            {
+                oldSubContent = File.ReadAllText(tmpFile);
+            }
+            catch (FileNotFoundException ex)
+            {
+                oldSubContent = ex.Message;
+            }
+
+            if (oldSubContent != subContent)
+            {
+                cntDown = 60;
+                this.timer_CntDwn.Enabled = true;
+            }
+
+            label_last_update.Text = System.DateTime.Now.ToString();
+            this.notifyIcon1.Text = "Kepi Crawler, last update at " + label_last_update.Text;
+
+            //return pageContent;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.Delete(tmpFile);
+            this.timer_5min.Stop();
+            this.timer_5min.Interval = 120000; // = 2 minutes
+            this.timer_5min.Start();
+            this.timer_CntDwn.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void timer_CntDwn_Tick(object sender, EventArgs e)
+        {
+            if ((cntDown < 55) && (subContent.Length == 0))
+            {
+                this.richTextBox1.Text = String.Format("No data to send at {0} (cancelled)", DateTime.Now.ToString());
+                this.timer_CntDwn.Enabled = false;
+                File.WriteAllText(tmpFile, subContent); // avoid a re-trigger in 5 minutes.
+            }
+            else if (cntDown > 0)
+            {
+                this.richTextBox1.Text = String.Format("Sending in {0} seconds:\n{1}", cntDown , subContent);
+                cntDown--;
+                switch (cntDown)
+                {
+                    case 59:
+                    case 54:
+                    case 44:
+                        this.Show();
+                        this.WindowState = FormWindowState.Normal;
+                        this.TopMost = true;
+                        this.CenterToScreen();
+                        this.TopMost = false;
+                        break;
+                }
+            }
+            else
+            {
+                this.richTextBox1.Text = String.Format("Sent at {0}:\n{1}", DateTime.Now.ToString(), subContent);
+                string emailTo = this.textBox_eMail.Text;
+                string originalValue = MyKepiCrawler.Properties.Settings.Default.Properties["MySendEmail"].DefaultValue as string;
+                if (emailTo == originalValue)
+                    emailTo = "";
+                try
+                {
+                    File.WriteAllText(tmpFile, subContent);
+
+                    if (emailTo.Length > 3)
+                    {
+                        string bodyEmail = subContent;
+
+                        Microsoft.Office.Interop.Outlook.Application app = new Microsoft.Office.Interop.Outlook.Application();
+                        Microsoft.Office.Interop.Outlook.MailItem eMail = app.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem);
+
+                        eMail.Subject = "Kepi Update V1";
+                        eMail.To = emailTo;
+                        eMail.Body = bodyEmail;
+                        ((Microsoft.Office.Interop.Outlook._MailItem)eMail).Send();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string errMsg = String.Format("Error sending 'Kepi Update' Email: {0} at {1}:\n{2}", ex.Message, DateTime.Now.ToString(), subContent);
+                    this.richTextBox1.Text = errMsg;
+                    File.WriteAllText(tmpFile, errMsg);
+                }
+                finally
+                {
+                    this.timer_CntDwn.Enabled = false;
+                }
+            }
+            Properties.Settings.Default.MySearchString = this.textBox2.Text;
+            Properties.Settings.Default.MySendEmail = this.textBox_eMail.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
