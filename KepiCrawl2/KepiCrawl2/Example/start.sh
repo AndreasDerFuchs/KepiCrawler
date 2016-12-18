397cb259dcc962a3aedca3ecfd8f73bad0c54f4b
Andreas Schlack <ask-giub14@schlack.ws>
2016-12-18 10:51:58 +0100

    Added an Example Directory. Please rename this directory and use as you please (but don't push it to git).

---
+++ KepiCrawl2/KepiCrawl2/Example/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+../bin/Debug/KepiCrawl2.exe Schule "kepi tuebingen" User 5b Pw Schnabel Title "Quentin    -    Quentin" -geometry 720x688-06+130 &
+sleep 5
+../bin/Debug/KepiCrawl2.exe Schule "kepi tuebingen" User 8d Pw Lechner  Title "Vincent    -    Vincent" -geometry 720x688+700+130 &
